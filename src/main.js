a5e8b09f31417d3195d1a4002007a414743df05a
Anatoliy Daniluyk <anatolydanilyuk2002@gmail.com>
2024-06-25 14:14:52 +0300

    add section reviews

---
+++ src/main.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: false,
+  speed: 500,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+  },
+  breakpoints: {
+    1280: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 80,
+    },
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 40,
+    },
+  },
+  on: {
+    init: function () {
+      updateNavigationButtons(this);
+    },
+    slideChange: function () {
+      updateNavigationButtons(this);
+    },
+  },
+});
+
+function updateNavigationButtons(swiper) {
+  const { isBeginning, isEnd } = swiper;
+  const prevButton = document.querySelector('.swiper-prev');
+  const nextButton = document.querySelector('.swiper-next');
+
+  if (isBeginning) {
+    prevButton.classList.add('inactive');
+  } else {
+    prevButton.classList.remove('inactive');
+  }
+
+  if (isEnd) {
+    nextButton.classList.add('inactive');
+  } else {
+    nextButton.classList.remove('inactive');
+  }
+}
