4425b8e29e662af92e8530092e5f61458e33a743
Anatoliy Daniluyk <anatolydanilyuk2002@gmail.com>
2024-06-28 11:07:59 +0300

    fix gallery

--- src/js/js-gallery.js
+++ src/js/js-gallery.js
@@ -1,15 +1,59 @@
-const swiper = new Swiper('.swiperGallery', {
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 2,
   loop: true,
   centeredSlides: true,
   speed: 800,
+  // breakpoints: {
+  //   1440: {
+  //     slidesPerView: 3.3,
+  //     spaceBetween: 80,
+  //   },
+  //   0: {
+  //     slidesPerView: 2.1,
+  //     spaceBetween: 15,
+  //   },
+  // },
   breakpoints: {
+    340: {
+      slidesPerView: 2.05,
+      spaceBetween: 8,
+    },
+    360: {
+      slidesPerView: 2.2,
+      spaceBetween: 10,
+    },
+    380: {
+      slidesPerView: 2.3,
+      spaceBetween: 12,
+    },
+    400: {
+      slidesPerView: 2.4,
+      spaceBetween: 14,
+    },
+    420: {
+      slidesPerView: 2.5,
+      spaceBetween: 16,
+    },
+    440: {
+      slidesPerView: 2.6,
+      spaceBetween: 18,
+    },
+    460: {
+      slidesPerView: 2.7,
+      spaceBetween: 20,
+    },
+    480: {
+      slidesPerView: 2.8,
+      spaceBetween: 22,
+    },
+    490: {
+      slidesPerView: 3,
+      spaceBetween: 22,
+    },
+
     1440: {
       slidesPerView: 3.3,
       spaceBetween: 80,
-    },
-    0: {
-      slidesPerView: 2.1,
-      spaceBetween: 15,
     },
   },
   autoplay: {
