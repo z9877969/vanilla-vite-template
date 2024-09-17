a738947b81719d7229518f06812719a5cba8ea97
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-09-17 15:50:19 +0300

    new html, scss, js

--- src/js/new.js
+++ src/js/new.js
@@ -1,14 +1,54 @@
-document.querySelectorAll('.question-title').forEach(item => {
-  item.addEventListener('click', () => {
-    const answer = item.nextElementSibling;
-    const toggle = item.querySelector('.toggle');
+// js/new.js
 
-    if (answer.style.display === 'block') {
-      answer.style.display = 'none';
-      toggle.textContent = '+';
-    } else {
-      answer.style.display = 'block';
-      toggle.textContent = '−';
-    }
+// відгук
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', () => {
+    const content = header.nextElementSibling;
+    const icon = header.querySelector('.toggle');
+
+    content.style.maxHeight
+      ? (content.style.maxHeight = null)
+      : (content.style.maxHeight = content.scrollHeight + 'px');
+
+    icon.textContent === '+'
+      ? (icon.textContent = '-')
+      : (icon.textContent = '+');
+  });
+});
+
+// питання
+const questionTitles = document.querySelectorAll('.question-title');
+questionTitles.forEach(title => {
+  title.addEventListener('click', () => {
+    const answer = title.nextElementSibling;
+    const icon = title.querySelector('.toggle');
+
+    answer.style.maxHeight
+      ? (answer.style.maxHeight = null)
+      : (answer.style.maxHeight = answer.scrollHeight + 'px');
+
+    icon.textContent === '+'
+      ? (icon.textContent = '-')
+      : (icon.textContent = '-');
+  });
+});
+
+// Горизонт. прокрут. відгук.
+const reviewsContainer = document.querySelector('.reviews-container');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+prevButton.addEventListener('click', () => {
+  reviewsContainer.scrollBy({
+    left: -reviewsContainer.clientWidth,
+    behavior: 'smooth',
+  });
+});
+
+nextButton.addEventListener('click', () => {
+  reviewsContainer.scrollBy({
+    left: reviewsContainer.clientWidth,
+    behavior: 'smooth',
   });
 });
