811750eccceccfe5eec00f183e7caa5cea338e58
Maksimka66 <maxfese@gmail.com>
2024-09-20 19:12:25 +0300

    faq section is done

--- src/js/new.js
+++ src/js/new.js
@@ -24,17 +24,24 @@ scrollLeftReviews.addEventListener('click', () => {
 const questionItems = document.querySelectorAll('.question-item');
 
 questionItems.forEach(item => {
-  item.querySelector('.question-title').addEventListener('click', () => {
-    const answer = item.querySelector('.answer-content');
+  const questionButton = item.querySelector('.question-button');
+  const plusIcon = item.querySelector('.plus-icon');
+  const answer = item.querySelector('.answer-content');
 
+  item.querySelector('.question-title').addEventListener('click', () => {
     if (answer.style.display === 'block') {
       answer.style.display = 'none';
+      plusIcon.classList.remove('rotate');
     } else {
       document.querySelectorAll('.answer-content').forEach(answerContent => {
         answerContent.style.display = 'none';
       });
+      document.querySelectorAll('.plus-icon').forEach(icon => {
+        icon.classList.remove('rotate');
+      });
 
       answer.style.display = 'block';
+      plusIcon.classList.add('rotate');
     }
   });
 });
