4d9fd4f3bdc0f3b7d6192b1caf1db35273de6b89
Maksimka66 <maxfese@gmail.com>
2024-09-20 06:57:56 +0300

    work with faq section

--- src/js/new.js
+++ src/js/new.js
@@ -5,7 +5,7 @@ const scrollRightReviews = document.querySelector('.right-reviews');
 const itemWidth = document.querySelector('.accordion-item').offsetWidth;
 
 const gap = 24;
-const scrollAmount = itemWidth + gap;
+const scrollAmount = itemWidth + gap * 2;
 
 scrollRightReviews.addEventListener('click', () => {
   reviewsList.scrollBy({
@@ -18,5 +18,23 @@ scrollLeftReviews.addEventListener('click', () => {
   reviewsList.scrollBy({
     left: -scrollAmount,
     behavior: 'smooth',
+  });
+});
+
+const questionItems = document.querySelectorAll('.question-item');
+
+questionItems.forEach(item => {
+  item.querySelector('.question-title').addEventListener('click', () => {
+    const answer = item.querySelector('.answer-content');
+
+    if (answer.style.display === 'block') {
+      answer.style.display = 'none';
+    } else {
+      document.querySelectorAll('.answer-content').forEach(answerContent => {
+        answerContent.style.display = 'none';
+      });
+
+      answer.style.display = 'block';
+    }
   });
 });
