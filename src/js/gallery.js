ba87a510b6ae1274b1c85437229f5e2d59bb832d
SSovelich <163519655+Ssovelich@users.noreply.github.com>
2024-12-20 21:21:52 +0200

    added game-features

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -5,42 +5,40 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const swiper = new Swiper('.swiper');
+// new Swiper('.swiper-gall');
 
-// const swiper = new Swiper('.swiper', {
-//   direction: 'horizontal',
-//   loop: true,
+const swiper = new Swiper('.mySwiper', {
+  modules: [Navigation, Keyboard, Mousewheel, Pagination],
+  speed: 1000,
 
-//   modules: [Navigation, Keyboard, Mousewheel, Pagination],
-//   speed: 1000,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    enabled: true,
+    eventsTarget: '.mySwiper',
+  },
 
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: true,
-//     pageUpDown: true,
-//   },
-//   mousewheel: {
-//     enabled: true,
-//     eventsTarget: '.swiper',
-//   },
-
-//   breakpoints: {
-//     320: {
-//       slidesPerView: 2,
-//       spaceBetween: 15,
-//     },
-//     1200: {
-//       slidesPerView: 3,
-//       spaceBetween: 36,
-//     },
-//   },
-// });
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 36,
+    },
+  },
+});
