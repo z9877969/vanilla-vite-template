f1ad3f345d38e182a49bde38fa9bc1ca070c2407
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2025-02-19 15:15:37 +0200

    Add Footer

--- global.styles.js
+++ global.styles.js
@@ -2,14 +2,15 @@ const variablesStyles = '@import "./src/scss/common/_vars.scss"';
 const breakpointsStyles = '@import "./src/scss/common/_breakpoints.scss"';
 const mixinsStyles = '@import "./src/scss/common/_mixins.scss"';
 const resetStyles = '@import "./src/scss/common/_reset.scss"';
+const base = '@import "./src/scss/common/_base.scss"';
 
 const styles = [
   variablesStyles,
   breakpointsStyles,
-  //   colorsStyles,
   mixinsStyles,
   //   baseStyles,
   resetStyles,
+  base,
 ];
 
 export const globalStylesOptions = styles.reduce((acc, i) => acc + i + ';', '');
