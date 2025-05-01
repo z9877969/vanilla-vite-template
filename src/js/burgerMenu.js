47d501799877ef93bf947f862d50d40d0e03555b
z9877969 <z9877969@ukr.net>
2025-05-01 18:47:25 +0300

    adds burger with dataAction selectors

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,1 +1,11 @@
+const openBtnEl = document.querySelector('[data-action="open"]');
+const closeBtnEl = document.querySelector('[data-action="close"]');
+const burgerMenuEl = document.querySelector('[data-visible]');
 
+openBtnEl.addEventListener('click', e => {
+  burgerMenuEl.dataset.visible = 'open';
+});
+
+closeBtnEl.addEventListener('click', e => {
+  burgerMenuEl.dataset.visible = 'close';
+});
